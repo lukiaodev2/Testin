f0227cba206b0161d665fc140b8ba11b28210be6
Lukiao <lukiao@Lukiaos-Mac-mini.local>
2024-11-08 15:56:22 -0500

    record

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,83 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect } from "react";
-import {
-  LocationRequestComponent,
-  VideoCameraRequestComponent,
-} from "./permissions.tsx";
 
 function App() {
-  const { registerForVideoCamera } = VideoCameraRequestComponent();
+  const [recording, setRecording] = React.useState(false);
+  const [videoURL, setVideoURL] = React.useState(null);
+  const mediaRecorderRef = React.useRef(null);
+  const videoRef = React.useRef(null);
+  const chunks = React.useRef([]);
 
-  const { registerForLocation } = LocationRequestComponent();
+  const startRecording = async () => {
+    // Solicitar permiso y acceso a la cámara
+    try {
+      if (videoRef && mediaRecorderRef) {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        videoRef.current.srcObject = stream;
 
-  const fetchCameraAndSetupListener = async () => {
-    const stream = await registerForVideoCamera();
-    if (stream) {
-      alert("otorgados");
+        // Configurar el MediaRecorder para grabar el video
+        mediaRecorderRef.current = new MediaRecorder(stream, {
+          mimeType: "video/webm",
+        });
+
+        mediaRecorderRef.current.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+            chunks.current.push(event.data);
+          }
+        };
+
+        mediaRecorderRef.current.onstop = () => {
+          const blob = new Blob(chunks.current, { type: "video/webm" });
+          setVideoURL(URL.createObjectURL(blob));
+          chunks.current = [];
+        };
+
+        mediaRecorderRef.current.start();
+      } else {
+        console.error("No se pudo acceder a la cámara");
+      }
+      setRecording(true);
+    } catch (error) {
+      console.error("Error al acceder a la cámara:", error);
     }
   };
 
-  const fetchLocation = async () => {
-    const loc = await registerForLocation(); // Espera a obtener la ubicación
+  const stopRecording = () => {
+    if (mediaRecorderRef && videoRef) {
+      mediaRecorderRef.current.stop();
+      const stream = videoRef.current.srcObject;
+      const tracks = stream.getTracks();
 
-    if (loc) {
-      const { latitude, longitude } = loc;
-      alert(`obtenido ${latitude}, ${longitude}`, );
+      tracks.forEach((track) => track.stop());
     }
-  };
 
-  useEffect(() => {
-    (async () => {
-      await fetchCameraAndSetupListener();
-      fetchLocation();
-    })();
-  }, []);
+    setRecording(false);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video ref={videoRef} autoPlay muted></video>
+        <div>
+          {!recording ? (
+            <button onClick={startRecording}>
+              Comenzar grabación
+            </button>
+          ) : (
+            <button onClick={stopRecording}>Detener grabación</button>
+          )}
+        </div>
+        {videoURL && (
+          <div>
+            <h3>Video Grabado:</h3>
+            <video src={videoURL} controls></video>
+            <a href={videoURL} download="video_grabado.webm">
+              Descargar video
+            </a>
+          </div>
+        )}
       </header>
     </div>
   );
